b8e3990dccf51ac1fe87f982eebf888f26c4d2c7
Jose Moreno <josem1988@gmail.com>
2017-04-22 19:59:06 +0200

    +Glass refraction test

---
+++ Assets/Game/Scripts/Rendering/GlassDistorsion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassDistorsion : MonoBehaviour
+{
+
+	void OnRenderImage (RenderTexture src, RenderTexture dst)
+	{
+		Graphics.Blit (src, dst, _distorsionMaterial);
+	}
+
+	[SerializeField]
+	private Material _distorsionMaterial;
+}
