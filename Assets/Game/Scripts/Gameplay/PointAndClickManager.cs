65e871ba3c5b22bf9f313e19b92405b2e555c724
Javi <javibm6@gmail.com>
2017-04-22 11:08:57 +0200

    * Añadida logica para clickar smart objects.

--- Assets/Game/Scripts/Gameplay/PointAndClickManager.cs
+++ Assets/Game/Scripts/Gameplay/PointAndClickManager.cs
@@ -1,4 +1,24 @@
 ﻿using UnityEngine;
 
-public class PointAndClickManager : MonoBehaviour {
+public class PointAndClickManager : MonoBehaviour
+{
+  public delegate void SmartObjectClickedHandler(SmartObject smartObject);
+  public static SmartObjectClickedHandler OnSmartObjectClicked;
+
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+
+      if (Physics.Raycast(ray, out hit))
+      {
+        if (hit.transform.gameObject.layer == LayerMask.NameToLayer(Constants.Layers.SmartObjects))
+        {
+          OnSmartObjectClicked(hit.transform.gameObject.GetComponent<SmartObject>());
+        } 
+      }
+    }
+  }
 }
