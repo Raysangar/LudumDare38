da28b5782877173019f79086d0f75eded4e8991c
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2017-04-22 20:58:47 +0200

    Player now moves using a navmesh

--- Assets/Game/Scripts/Gameplay/PlayerMovementController.cs
+++ Assets/Game/Scripts/Gameplay/PlayerMovementController.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 using System.Collections;
 
+[RequireComponent (typeof (NavMeshAgent))]
 public class PlayerMovementController : MonoBehaviour
 {
   private void Start ()
   {
+    navMeshAgent = GetComponent<NavMeshAgent> ();
     cachedTransform = transform;
     PointAndClickManager.OnSmartObjectClicked += OnSmartObjectClicked;
   }
@@ -13,38 +16,18 @@ public class PlayerMovementController : MonoBehaviour
   {
     StopAllCoroutines ();
     target = smartObject;
-    movementDirection = (smartObject.SmartPosition.transform.position - cachedTransform.position).normalized;
     StartCoroutine (GoInteractWithTarget ());
   }
 
   private IEnumerator GoInteractWithTarget ()
   {
-    // Look to smart position
+    navMeshAgent.destination = target.SmartPosition.position;
+
+    yield return new WaitUntil (IsCloseToTarget);
+
     float time = 0;
     Vector3 origin = cachedTransform.forward;
-    float duration = (movementDirection - origin).magnitude / angularSpeed;
-    if (!IsCloseToTarget)
-    {
-      while (!LookingToSmartPosition)
-      {
-        cachedTransform.forward = Vector3.Lerp (origin, movementDirection, Mathf.InverseLerp (0, duration, time));
-        yield return null;
-        time += Time.deltaTime;
-      }
-    }
-
-    // Move to smart position
-    Vector3 movementPerSecond = movementDirection * speed;
-    while (!IsCloseToTarget)
-    {
-      cachedTransform.position += movementPerSecond * Time.deltaTime;
-      yield return null;
-    }
-
-    // Look to smart object
-    time = 0;
-    origin = cachedTransform.forward;
-    duration = (target.SmartPosition.forward - origin).magnitude / angularSpeed;
+    float duration = (target.SmartPosition.forward - origin).magnitude / angularSpeed;
     while (!LookingToSmartObject)
     {
       cachedTransform.forward = Vector3.Lerp (origin, target.SmartPosition.forward, Mathf.InverseLerp (0, duration, time));
@@ -55,14 +38,6 @@ public class PlayerMovementController : MonoBehaviour
     target.Interact ();
   }
 
-  private bool LookingToSmartPosition
-  {
-    get
-    {
-      return (movementDirection - cachedTransform.forward).sqrMagnitude < angularThreshold;
-    }
-  }
-
   private bool LookingToSmartObject
   {
     get
@@ -71,16 +46,10 @@ public class PlayerMovementController : MonoBehaviour
     }
   }
 
-  private bool IsCloseToTarget
+  private bool IsCloseToTarget ()
   {
-    get
-    {
-      return (target.SmartPosition.position - cachedTransform.position).sqrMagnitude < distanceThreshold;
-    }
+    return (target.SmartPosition.position - cachedTransform.position).sqrMagnitude < distanceThreshold;
   }
-
-  [SerializeField]
-  private float speed;
 
   [SerializeField]
   private float angularSpeed;
@@ -92,6 +61,6 @@ public class PlayerMovementController : MonoBehaviour
   private float angularThreshold;
 
   private SmartObject target;
-  private Vector3 movementDirection;
   private Transform cachedTransform;
+  private NavMeshAgent navMeshAgent;
 }
