c21f4f8c0c7594b505d6a088b3659ccbedf0f226
Javi <javibm6@gmail.com>
2017-04-22 18:13:16 +0200

    Añadidos todos los smartobjetcs.

---
+++ Assets/Game/Scripts/Gameplay/SmartObjects/GardenSmartObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GardenSmartObject : SmartObject
+{
+  public override void Interact()
+  {
+    base.Interact();
+    print("Interaction With Garden");
+  }
+}
---
+++ Assets/Game/Scripts/Gameplay/SmartObjects/HouseSmartObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HouseSmartObject : SmartObject
+{
+  public int Level
+  {
+    get { return level; }
+  }
+
+  public override void Interact()
+  {
+    base.Interact();
+    print("Interaction With House");
+  }
+
+  private int level;
+}
