0370fbc3bcf153e5802b221d7a98c6239a456d2a
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2017-04-22 18:31:30 +0200

    Player HUD manager implemented

--- Assets/Game/Scripts/Gameplay/PlayerManager.cs
+++ Assets/Game/Scripts/Gameplay/PlayerManager.cs
@@ -9,6 +9,22 @@ public class PlayerManager : MonoBehaviour {
     get { return instance; }
   }
 
+  public int CurrentFood
+  {
+    get
+    {
+      return currentFood;
+    }
+  }
+
+  public int CurrentWater
+  {
+    get
+    {
+      return currentWater;
+    }
+  }
+
   public void Drink (int waterAmount)
   {
     currentWater += waterAmount;
@@ -25,18 +41,21 @@ public class PlayerManager : MonoBehaviour {
     currentFood -= foodLostEveryStage;
     currentWater -= waterLostEveryStage;
 
-    if (currentFood <= 0 || currentWater <= 0)
+    if (currentFood < 0) { currentFood = 0; }
+    if (currentWater < 0) { currentWater = 0; }
+    
+    if (currentFood == 0 || currentWater == 0)
     {
       OnPlayerDied ();
-    } 
+    }
   }
 
   private void Awake ()
   {
+    instance = this;
     currentFood = initialFood;
     currentWater = initialWater;
   }
-
 
   [SerializeField]
   private int initialFood;
