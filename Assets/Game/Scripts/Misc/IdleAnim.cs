adb10fbb50a9ec5f6aa00f66c96e8aa727b9f65a
Selbryn <jasnitro@gmail.com>
2017-04-23 15:26:17 +0200

    * Particles modified. + Added IdleAnim.

---
+++ Assets/Game/Scripts/Misc/IdleAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleAnim : MonoBehaviour {
+
+	public float	maxRotationX;
+	public float	maxRotationY;
+	public float	maxRotationZ;
+	public float	speed;
+
+	private Vector3 initRotation;
+	private Vector3 newRot;
+	private float 	sinSpeed;
+
+	void Start () {
+
+		initRotation = transform.rotation.eulerAngles;
+	}
+
+	void Update () {
+
+		sinSpeed = Mathf.Sin (Time.time * speed);
+		newRot = new Vector3 (sinSpeed * maxRotationX, sinSpeed * maxRotationY, sinSpeed * maxRotationZ);
+		transform.rotation = Quaternion.Euler(initRotation.x + newRot.x, initRotation.y + newRot.y, initRotation.z + newRot.z);
+	}
+}
