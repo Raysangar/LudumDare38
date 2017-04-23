7df2d396aafe3146c723f11d06c4700919f770a7
Jose Moreno <josem1988@gmail.com>
2017-04-23 19:11:51 +0200

    +Sounds and sounds

---
+++ Assets/Game/Scripts/Gameplay/AudioManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+	[System.Serializable]
+	public struct AudioStruct
+	{
+		public AudioType audioType;
+		public AudioSource[] audioClips;
+	}
+
+	public enum AudioType
+	{
+		Default,
+		Main,
+		Gameover,
+		Wood,
+		Drink,
+		Eat,
+		Orchard,
+		Sheep,
+		TakeWater,
+		Wrong,
+		Build,
+		UIButton
+	}
+
+
+	void Awake ()
+	{
+		DontDestroyOnLoad (gameObject);
+		PlaySound (AudioType.Main);
+
+		PlayerManager.OnPlayerDrink -= OnPlayerDrink;
+		PlayerManager.OnPlayerEat -= OnPlayerEat;
+		SmartObject.OnPlayerInteraction -= OnPlayerInteraction;
+		SmartObjectsManager.OnPlayerUpgradeMonument -= OnPlayerUpgradeMonument;
+		SmartObjectsManager.OnPlayerUpgradeHouse -= OnPlayerUpgradeHouse;
+		SmartObjectsManager.OnPlayerIrrigate -= OnPlayerIrrigate;
+		SmartObjectsManager.OnPlayerBuildRanch -= OnPlayerBuildRanch;
+		SmartObjectsManager.OnPlayerBuildGarden -= OnPlayerBuildGarden;
+
+		PlayerManager.OnPlayerDrink += OnPlayerDrink;
+		PlayerManager.OnPlayerEat += OnPlayerEat;
+		SmartObject.OnPlayerInteraction += OnPlayerInteraction;
+		SmartObjectsManager.OnPlayerUpgradeMonument += OnPlayerUpgradeMonument;
+		SmartObjectsManager.OnPlayerUpgradeHouse += OnPlayerUpgradeHouse;
+		SmartObjectsManager.OnPlayerIrrigate += OnPlayerIrrigate;
+		SmartObjectsManager.OnPlayerBuildRanch += OnPlayerBuildRanch;
+		SmartObjectsManager.OnPlayerBuildGarden += OnPlayerBuildGarden;
+
+
+	}
+
+	private void PlaySound (AudioType audioType, bool loop = false)
+	{
+		if (!_audios [(int)audioType].audioClips [0].isPlaying) {
+			_audios [(int)audioType].audioClips [0].loop = loop;
+			_audios [(int)audioType].audioClips [0].Play ();	
+		}			
+	}
+
+	private void OnPlayerDrink ()
+	{
+		PlaySound (AudioType.Drink);
+	}
+
+	private void OnPlayerEat ()
+	{
+		PlaySound (AudioType.Eat);
+	}
+
+	private void OnPlayerUpgradeMonument ()
+	{
+		//PlaySound (AudioType.Eat);
+	}
+
+	private void OnPlayerUpgradeHouse ()
+	{
+		PlaySound (AudioType.Build);
+	}
+
+	private void OnPlayerBuildRanch ()
+	{
+		PlaySound (AudioType.Build);
+	}
+
+	private void OnPlayerBuildGarden ()
+	{
+		PlaySound (AudioType.Build);
+	}
+
+	private void OnPlayerIrrigate ()
+	{
+		PlaySound (AudioType.TakeWater);
+	}
+
+	private void OnPlayerInteraction (SmartObject so)
+	{
+		switch (so.Type) {
+		case SmartObject.ObjectType.Wood:
+			PlaySound (AudioType.Wood);
+			break;
+		case SmartObject.ObjectType.Ranch:
+			PlaySound (AudioType.Sheep);
+			break;
+		case SmartObject.ObjectType.Garden:
+			PlaySound (AudioType.Orchard);
+			break;
+		case SmartObject.ObjectType.Water:
+			PlaySound (AudioType.TakeWater);
+			break;
+		}
+	}
+
+
+	[SerializeField]
+	private AudioStruct[] _audios;
+
+}
