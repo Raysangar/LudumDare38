013f0e0be9fb3219d6a39799938940fad5142e0a
Javi <javibm6@gmail.com>
2017-04-24 21:19:47 +0200

    Añadido script de Twitter.

---
+++ Assets/Game/Scripts/Utils/Twitter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Twitter : MonoBehaviour {
+  private const string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+  private const string TWEET_LANGUAGE = "en";
+  public static string descriptionParam;
+  private static string appStoreLink = "https://raysangar.github.io/SurviBall/";
+  private string hashtag = "#surviball";
+
+  public void ShareToTW(string linkParameter)
+  {
+    string nameParameter = "I have discovered the truth in " + ActionsManager.Instance.ActionsMadeByPlayerOnPreviousStages.Count + ". Can you unveil the secrets of the island? ";
+    Application.OpenURL(TWITTER_ADDRESS +
+       "?text=" + WWW.EscapeURL(nameParameter + "\n" + descriptionParam + "\n" + "Get the Game:\n" + appStoreLink + " " + hashtag));
+  }
+}
